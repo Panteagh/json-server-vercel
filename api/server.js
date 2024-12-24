1f7a109808278680566579b8b8e9ec5fbe63e53b
Pantea ghaffari <panteagh.03@gmail.com>
2024-12-24 20:38:56 +0330

    ..

--- api/server.js
+++ api/server.js
@@ -22,10 +22,10 @@ server.use('/marketPlaceNFTs', express.static(path.join(__dirname, 'public/marke
 server.use('/Avatars', express.static(path.join(__dirname, 'public/Avatars')));
 
 // Add this before server.use(router)
-// server.use(jsonServer.rewriter({
-//     '/api/*': '/$1',
-//     '/blog/:resource/:id/show': '/:resource/:id'
-// }))
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
